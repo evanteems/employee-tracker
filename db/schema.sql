c92f602ee5db639e07a7aa1866b37feb6dda1cd7
Evan Teems <evanteems40@yahoo.com>
2021-03-07 13:19:01 -0800

    schema.sql finished

--- db/schema.sql
+++ db/schema.sql
@@ -8,4 +8,27 @@ DROP TABLE IF EXISTS departments;
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL,
-)
+    PRIMARY KEY (id)
+);
+
+-- ROLES START HERE
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(7.2),
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);
+
+-- EMPLOYEE HERE
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCGAR(30),
+    role_id INT,
+    manager_id INT,
+    CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_employees FOREIGN Key (manager_id) REFERENCES employees(id) ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);
